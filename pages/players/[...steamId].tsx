39e05d74b6113475875d5b68c68c8affd5b7b85e
Curia <admin@curiadesign.net>
2021-02-23 22:35:29 +1100

    Logic to handle players page children

--- pages/players/[...steamId].tsx
+++ pages/players/[...steamId].tsx
@@ -1,7 +1,10 @@
 import { gql } from '@apollo/client';
+import { Select } from '@chakra-ui/react';
 import Wrapper from '@components/Layout/Wrapper';
 import { PlayersHeader } from '@components/Players';
 import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 import client from '../../apolloConfig';
 import { Player_player } from '../../types/Player';
@@ -9,19 +12,75 @@ import { Player_player } from '../../types/Player';
 interface PlayerProps {
   steamId: string;
   player: Player_player;
+  path: string;
 }
 
-const PlayerProfile: React.FC<PlayerProps> = ({ steamId, player }) => {
+const PlayerProfileRoutes = ({ routes }) => {
+  const [appRoutes, setAppRoutes] = useState(routes);
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    const newState = [...appRoutes];
+    newState.map((item) => {
+      item.isActive = false;
+    });
+    newState[value].isActive = true;
+
+    setAppRoutes([...newState]);
+    router.push(appRoutes[value].path);
+  };
+  return (
+    <div>
+      <Select
+        placeholder="Select option"
+        onChange={handleChange}
+        defaultValue={appRoutes.findIndex((item) => item.isActive)}>
+        {appRoutes.map((route, index) => (
+          <option value={index} key={index}>
+            {route.name}
+          </option>
+        ))}
+      </Select>
+    </div>
+  );
+};
+
+const PlayerProfile: React.FC<PlayerProps> = ({ steamId, player, path }) => {
+  const routes = [
+    {
+      path: `/players/${steamId}`,
+      name: 'Overview',
+      isActive: false
+    },
+    {
+      path: `/players/${steamId}/matches`,
+      name: 'Matches',
+      isActive: false
+    },
+    {
+      path: `/players/${steamId}/heroes`,
+      name: 'Heroes',
+      isActive: false
+    }
+  ];
+  routes.map((route) => {
+    if (route.path === path) {
+      route.isActive = true;
+    }
+  });
+
   return (
     <Wrapper>
       <PlayersHeader player={player} steamId={steamId} />
+      <PlayerProfileRoutes routes={routes} />
     </Wrapper>
   );
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const steamId = context.params.steamId[0];
-  const path = context.params.steamId;
+  const path = context.resolvedUrl;
 
   const { data } = await client.query({
     query: gql`
